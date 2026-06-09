91cc38772e6cbcb2e4bbc5c10f1c77cdf903e92e
syetolog2011-sudo <syetolog.2011@gmail.com>
2026-06-10 00:37:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
-const SUPABASE_URL = 'ТВОЙ_SUPABASE_URL'
-const SUPABASE_ANON_KEY = 'ТВОЙ_SUPABASE_ANON_KEY'
+const SUPABASE_URL = 'https://otibfsqphueechyhrfef.supabase.co/rest/v1/'
+const SUPABASE_ANON_KEY = 'sb_publishable_rjhxwSlY6YUh5QZ5VvKNzA_jPRMruCp'
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
 
 // Данные Телеграма
